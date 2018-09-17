b245a2c3c4b5c87278077cd8008acfe9e31fd6c1
neth <nethwebdev@gmail.com>
2018-09-17 12:46:14 -0700

    event listeners are set when page is ready

--- js/script.js
+++ js/script.js
@@ -1,24 +1,29 @@
 'use strict';
 
 $(document).ready(() => {
-  console.log('Ready!');
+  addEventListeners();
 });
 
-$('input[name="text-input-error"]').on('input', function(e) {
-  if (e.target.value) {
-    $(this).removeClass('invalid-form');
-    $('.error-text').hide();
-  } else {
-    $(this).addClass('invalid-form');
-    $('.error-text').show();
-  }
-});
+const addEventListeners = () => {
+  // add event listener on text input with error validation
+  $('input[name="text-input-error"]').on('input', function(e) {
+    if (e.target.value) {
+      $(this).removeClass('invalid-form');
+      $('.error-text').hide();
+    } else {
+      $(this).addClass('invalid-form');
+      $('.error-text').show();
+    }
+  });
 
-$('.checkbox-sprite').on('click', function(e) {
-  $(this).toggleClass('checkbox-sprite-selected');
-});
+  // add event listeners on checkboxes
+  $('.checkbox-sprite').on('click', function(e) {
+    $(this).toggleClass('checkbox-sprite-selected');
+  });
 
-$('.radio-sprite').on('click', function(e) {
-  $('.radio-sprite').removeClass('radio-sprite-selected');
-  $(this).addClass('radio-sprite-selected');
-})
+  // add event lisenters on radio buttons
+  $('.radio-sprite').on('click', function(e) {
+    $('.radio-sprite').removeClass('radio-sprite-selected');
+    $(this).addClass('radio-sprite-selected');
+  });
+}
